45a12a50cc9accc4198b28f57ac56ae6ecb4e19f
mygit <784778060@qq.com>
2019-05-29 22:09:18 +0800

    新建footer

--- src/components/subNav.vue
+++ src/components/subNav.vue
@@ -17,38 +17,159 @@
         <el-carousel-item v-for="(item,index) in listData" :key="item" >
           <el-card :body-style="{ padding: '0px'}" style="text-align: center;background-color: #fff">
 
-            <div style="padding: 14px; display: inline-block;text-align: left">
-              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div style="padding: 14px; display: inline-block;text-align: left" class="blockBox">
+              <div class="imgBox">
+                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+              </div>
               <br><span>{{item[index].name}}</span>
               <div class="bottom clearfix">
                 <time class="time">{{ item[index].liang }}</time>
               </div>
+              <i class="el-icon-caret-right"></i>
             </div>
-            <div style="padding: 14px; display: inline-block ;text-align: left">
-              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div style="padding: 14px; display: inline-block ;text-align: left" class="blockBox">
+              <div class="imgBox">
+                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+              </div>
               <br><span>{{item[index].name}}</span>
               <div class="bottom clearfix">
                 <time class="time">{{ item[index].liang }}</time>
               </div>
+              <i class="el-icon-caret-right"></i>
             </div>
-            <div style="padding: 14px; display: inline-block;text-align: left">
-              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div style="padding: 14px; display: inline-block;text-align: left" class="blockBox">
+              <div class="imgBox">
+                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+              </div>
               <br><span>{{item[index].name}}</span>
               <div class="bottom clearfix">
                 <time class="time">{{ item[index].liang }}</time>
               </div>
+              <i class="el-icon-caret-right"></i>
             </div>
-            <div style="padding: 14px; display: inline-block;text-align: left">
-              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div style="padding: 14px; display: inline-block;text-align: left" class="blockBox">
+              <div class="imgBox">
+                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+              </div>
               <br><span>{{item[index].name}}</span>
               <div class="bottom clearfix">
                 <time class="time">{{ item[index].liang }}</time>
               </div>
+              <i class="el-icon-caret-right"></i>
             </div>
           </el-card>
         </el-carousel-item>
       </el-carousel>
       </div>
+    <h2>新歌首发</h2>
+    <div class="subNav">
+      <ul>
+        <li>最新</li>
+        <li>内地</li>
+        <li>港台</li>
+        <li>欧美</li>
+        <li>韩国</li>
+        <li>日本</li>
+        <span class="playAll"><i class="el-icon-caret-right"></i>播放全部</span>
+      </ul>
+    </div>
+    <el-carousel indicator-position="outside" :autoplay="false" height="380px" style="background-color: #fff" trigger="click">
+      <el-carousel-item v-for="item in xingeData" :key="item">
+        <ul class="gequMain">
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+          <li>
+            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png">
+            <div class="gequBox">
+              <h3>{{item.name}}</h3>
+              <span>{{item.zuozhe}}</span>
+            </div>
+            <div class="time">
+              {{item.time}}
+            </div>
+          </li>
+
+        </ul>
+      </el-carousel-item>
+    </el-carousel>
     </div>
 </template>
   </div>
@@ -101,6 +222,23 @@
                 { name:"假日煲剧:2019上班年韩剧指南0",
                   liang:"播放量:7.43万"}
               ]
+            ],
+            xingeData:[
+              {
+                name:'灿烂的你',
+                zuozhe:'汪峰',
+                time:'05:33'
+              },
+              {
+                name:'灿烂的你',
+                zuozhe:'汪峰',
+                time:'05:33'
+              },
+              {
+                name:'灿烂的你',
+                zuozhe:'汪峰',
+                time:'05:33'
+              }
             ]
           }
       }
@@ -120,6 +258,7 @@
     }
     .subNav {
       ul {
+        position: relative;
         overflow: hidden;
         /*background-color: pink;*/
         list-style: none;
@@ -132,15 +271,88 @@
     }
   }
   /*滑块*/
-  .el-carousel__button {
-    width: 20px !important;
-    height: 20px !important;
-    border-radius: 50% !important;
+  .blockBox {
+    position: relative;
+    .imgBox {
+      overflow: hidden;
+      img {
+        transition: all 0.5s;
+        border: 1px solid #ccc;
+      }
+    }
+
+    &:hover {
+      img {
+
+        transform: scale(1.1);
+      }
+      i {
+        transition: all 5s;
+        display: block;
+      }
+
+    }
+    i {
+      display: none;
+      position: absolute;
+      top: 50%-20;
+      left: 50%-10;
+      width: 50px;
+      height: 50px;
+      box-sizing: border-box;
+      padding-top: 10px;
+      text-align: center;
+      font-size: 30px;
+      background-color: #fff;
+      border-radius: 50%;
+      color: #aaa;
+    }
   }
-  .el-carousel__arrow {
-    border-radius: 2px !important;
+/*新歌首发*/
+  .playAll {
+    position: absolute;
+    left: 20%;
+    top: 30%;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    padding: 5px 20px;
+    i {
+      font-size: 20px;
+      color: #ccc;
+      vertical-align: -2px;
+    }
   }
-
-
-
+.gequMain {
+  overflow: hidden;
+  list-style: none;
+  text-align: center;
+  li {
+    /*float: left;*/
+    display: inline-block;
+    border-bottom: 1px solid #ccc;
+    padding: 10px 0;
+    margin: 0 10px;
+    img {
+      width: 86px;
+      margin-right: 5px;
+      vertical-align: -30px;
+    }
+    .gequBox {
+      display: inline-block;
+      vertical-align: -10px;
+      width: 236px;
+      text-align: left;
+      h3 {
+        font-weight: 400;
+        font-size: 16px;
+      }
+      span {
+        color: #ccc;
+      }
+    }
+    .time {
+      display: inline-block;
+    }
+  }
+}
 </style>
